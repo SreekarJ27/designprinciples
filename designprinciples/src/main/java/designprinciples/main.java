32682b01bb28b344083c2a402afac1ff47d478b3
SreekarJ27 <57980569+SreekarJ27@users.noreply.github.com>
2020-03-10 13:09:40 +0530

    sreecommit

---
+++ designprinciples/src/main/java/designprinciples/main.java
@@ -0,0 +1,16 @@
+package designprinciples;
+
+public class main {
+public static void main(String args[])
+{
+	Calculator c=new Calculator();
+	System.out.println(c.add(3, 4));
+	System.out.println(c.div(3, 4));
+	System.out.println(c.mul(3, 4));
+	System.out.println(c.sub(3, 4));
+	
+	
+	
+	
+}
+}
